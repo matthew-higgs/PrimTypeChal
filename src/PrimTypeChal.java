81d7d623daed7c5068b31f2e9cdc4cdbf9bd1c32
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-05 13:55:10 -0400

    initial commit

--- src/PrimTypeChal.java
+++ src/PrimTypeChal.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class PrimTypeChal {
+public class PrimTypeChal {
+    public static void main(String[] args) {
+        byte myByte = 116;
+        short myShort = 21000;
+        int myInt = 31586578;
+        long myLong = 80000 - 5 * (myByte + myShort + myInt);
+        System.out.println(myLong);
+    }
 }
